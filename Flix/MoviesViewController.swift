6d24b5fa2067a860621ca7f17066bf1b2b0e8c2f
Kimmiko James <kimmikojames@gmail.com>
2019-10-31 13:09:32 -0700

    Created trailer view

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -37,12 +37,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 
                 self.tableView.reloadData()
                 
-               // print(dataDictionary)
-                
-                // TODO: Get the array of movies
-                // TODO: Store the movies in a property to use elsewhere
-                // TODO: Reload your table view data
-                
             }
         }
         task.resume()
---
+++ Flix/TrailerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TrailerViewController.swift
+//  Flix
+//
+//  Created by Miko James on 10/31/19.
+//  Copyright © 2019 Miko James. All rights reserved.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+
+    @IBOutlet weak var trailerView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
